f0cb42f8afccdae1a30ae9ce4ef829edd0bbf977
iyttor <issar.tzachor@mail.huji.ac.il>
2023-04-03 17:05:02 +0300

    yamls scan load from checkpoint and not from end model

--- utils/common_config.py
+++ utils/common_config.py
@@ -88,7 +88,7 @@ def get_model(p, pretrain_path=None):
         state = torch.load(pretrain_path, map_location='cpu')
         
         if p['setup'] == 'scan': # Weights are supposed to be transfered from contrastive training
-            missing = model.load_state_dict(state, strict=False)
+            missing = model.load_state_dict(state['model'], strict=False)
             assert(set(missing[1]) == {
                 'contrastive_head.0.weight', 'contrastive_head.0.bias', 
                 'contrastive_head.2.weight', 'contrastive_head.2.bias'}
